91949308becbd8517a75db46c0878ab0da925d2a
Ian Diaz <idiaz@itba.edu.ar>
2019-09-13 15:54:25 -0300

    Se agregaron los diagramas paramétricos de polos y ceros

--- Ex5/Calculus/test.py
+++ Ex5/Calculus/test.py
@@ -87,32 +87,33 @@ K = Symbol('K', real=True, positive =True)
 
 s = Symbol('s')
 f = Symbol('f')
-den = 1/(s*C1)+R2*K+R2*(1-K)
+# den = 1/(s*C1)+R2*K+R2*(1-K)
 
-z1 = (1/(s*C1)*R2*(1-K))/(den)
-z2 = (1/(s*C1)*R2*K)/(den)
-z3 = R2**2 * K *(1-K)/(den)
+# z1 = (1/(s*C1)*R2*(1-K))/(den)
+# z2 = (1/(s*C1)*R2*K)/(den)
+# z3 = R2**2 * K *(1-K)/(den)
 
-za = z2+R1
-zb = z3 + (1/(s*C2))
-zc = z1 + R1
+# za = z2+R1
+# zb = z3 + (1/(s*C2))
+# zc = z1 + R1
 
-nom = (za*zb+zb*zc+zc*za)
+# nom = (za*zb+zb*zc+zc*za)
 
-zk1 = nom/zc
-zk2 = nom / za
-zk3 = nom/zb
+# zk1 = nom/zc
+# zk2 = nom / za
+# zk3 = nom/zb
 
-zalpha = (zk1 * R3)/(zk1+R3)
-zbeta = (zk2*R3)/(zk2+R3)
+# zalpha = (zk1 * R3)/(zk1+R3)
+# zbeta = (zk2*R3)/(zk2+R3)
 
-H = - sy.simplify((zbeta/zalpha))
+# H = - sy.simplify((zbeta/zalpha))
 
 
 
 wo = sy.sqrt((2*R1+R2)/(100*C2**2 *R1*R2**2))
 wo = sy.simplify(wo)
 
+A = 1/wo**2
 
 C=2*R1+R2
 B=-C2*K**2*R2**2-9*C2*K*R2**2+C2*R1**2+31*C2*R1*R2+10*C2*R2**2
@@ -120,9 +121,8 @@ E=-C2*K**2 *R2**2+11*C2*K*R2**2+C2*R1**2+31*C2*R1*R2
 
 Qz= C/(B *wo)
 Qp = C/(E*wo)
-A=Qp/Qz
-A = sy.simplify(A)
-
+#A=Qp/Qz
+#A = sy.simplify(A)
 
 #H = substitute(H)
 
@@ -130,7 +130,44 @@ A = sy.simplify(A)
 
 #plotExpress(Qz,Qp,A)
 
-print(H)
+#print(H)
 #plotH(H)
 
+Polo1 = - wo/(2*Qp) + (wo/2 * sy.sqrt(1/(Qp**2) -4))
+Polo2 = - wo/(2*Qp) - (wo/2 * sy.sqrt(1/(Qp**2)-4))
 
+Cero1 = - wo/(2*Qz) + (wo/2 * sy.sqrt(1/(Qz**2) -4))
+Cero2 = - wo/(2*Qz) - (wo/2 * sy.sqrt(1/(Qz**2) -4))
+
+
+
+def plotPolesAndCeros(pl1,pl2,z1,z2):
+    steps = 30
+    k = np.linspace(0.7,1.0,num=steps)
+    pl1 = substitute(pl1)
+    pl2 = substitute(pl2)
+    z1 = substitute(z1)
+    z2 = substitute(z2)
+    pl1k = []
+    pl2k = []
+    z1k = []
+    z2k = []
+    for u in range(0,steps):
+        pl1k.append(pl1.subs(K,k[u]))
+        pl2k.append(pl2.subs(K,k[u]))
+        z1k.append(z1.subs(K,k[u]))
+        z2k.append(z2.subs(K,k[u]))
+    for i in range(0,steps):
+        #print(f"pl1k:{pl1k}")
+        #print(f"pl2k:{pl2k}")
+        #print(f"z1k:{z1k}")
+        #print(f"z2k:{z2k}")
+
+        plt.scatter(sy.re(pl1k[i]),sy.im(pl1k[i]),alpha=0.5,marker="x")
+        plt.scatter(sy.re(pl2k[i]),sy.im(pl2k[i]),alpha=0.5,marker="x")
+        plt.scatter(sy.re(z1k[i]),sy.im(z1k[i]),alpha=0.5,marker="o")
+        plt.scatter(sy.re(z2k[i]),sy.im(z2k[i]),alpha=0.5,marker="o")
+    plt.grid(True)    
+    plt.show()
+
+plotPolesAndCeros(Polo1,Polo2,Cero1,Cero2)
